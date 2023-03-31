e3aa4255c9cdc8d86490cfc55ebfe792d7119c24
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-03-31 11:38:59 -0300

    .
    .

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ window.addEventListener('load', () => {
         const name = document.querySelector('#name').value;
         const email = document.querySelector('#email').value;
         const message = document.querySelector('#message').value;
+        const telefono = document.querySelector('#telefono').value;
 
-        if (name !== '' && email !== '' && message !== '') {
+        if (name !== '' && email !== '' && message !== '' && telefono !== '') {
             // ok
+            document.querySelector('#mensaje').classList.add('show-mensaje');
             document.querySelector('#user-name').innerHTML = name;
             document.querySelector('#user-email').innerHTML = email;
+            document.querySelector('#user-telefono').innerHTML = telefono;
             document.querySelector('#user-message').innerHTML = message;
         } else {
             // error
@@ -29,7 +32,13 @@ function getUser() {
         })
         .then((response) => {
             const userData = response.results[0].name;
+            const foto =  response.results[0].picture.large;
+            const userImg = document.querySelector('.user-img');
+            userImg.setAttribute('src', foto);
             document.querySelector('#user-name').innerHTML = `${userData.title}. ${userData.first} ${userData.last}`;
         })
-        .catch((error) => console.log('aja', error));
+        .catch((error) =>{ 
+            console.log('aja', error);
+            document.querySelector('#error').classList.add('show-error');
+        });
 }
